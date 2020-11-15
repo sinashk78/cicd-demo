e5dfec7e431e01ff119fd966d46fea170b82c465
sinashk78 <sina.sh.k78@gmail.com>
2020-11-15 18:44:08 +0330

    last minute change

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,5 +10,5 @@ it("should respond with Hello World!", async () => {
 it("should respond with a status of 200", async () => {
   const response = await request(app).get("/hello").send();
 
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(400);
 });
